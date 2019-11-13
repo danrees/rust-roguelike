4b13ed713562076d008cac3165c4012236163252
Daniel Rees <daniel.rees@autodata.net>
2019-11-13 16:17:29 -0500

    https://aimlesslygoingforward.com/blog/2019/02/09/writing-a-rust-roguelike-for-the-desktop-and-the-web/ tiles

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+//use quicksilver::prelude::{State, Settings};
+//use quicksilver::lifecycle::{Window, Event};
+
+use quicksilver::{
+    geom::{Shape,Vector},
+    graphics::{Background::Img, Color, Font, FontStyle, Image},
+    lifecycle::{run, Asset, Settings, State, Window},
+    Future, Result,
+};
+
+struct Game {
+    title: Asset<Image>,
+    mononoki_font_info: Asset<Image>,
+}
+
+impl State for Game {
+    fn new() -> Result<Self> where Self: Sized {
+        let font_mononoki = "mononoki-Regular.ttf";
+
+        let title = Asset::new(Font::load(font_mononoki).and_then(|font| {
+            font.render("Quicksilver Roguelike", &FontStyle::new(72.0, Color::BLACK))
+        }));
+
+        let mononoki_font_info = Asset::new(Font::load(font_mononoki).and_then(|font| {
+            font.render(
+                "Mononokie font by Matthias Tellen, terms: SIL Open Font License 1.1",
+                &FontStyle::new(20.0, Color::BLACK),
+            )
+        }));
+        Ok(Self{
+            title,
+            mononoki_font_info,
+        })
+    }
+
+    fn update(&mut self, _window: &mut Window) -> Result<()> {
+        Ok(())
+    }
+
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::WHITE)?;
+
+        self.title.execute(|image| {
+            window.draw(
+                &image.area().with_center((window.screen_size().x as i32 / 2, 40)),
+                Img(&image),
+            );
+            Ok(())
+        })?;
+
+        self.mononoki_font_info.execute(|image| {
+            window.draw(
+                &image.area().translate((2, window.screen_size().y as i32 - 60)),
+                Img(&image),
+            );
+            Ok(())
+        })?;
+
+        Ok(())
+    }
+
+}
+
+fn main() {
+    let settings = Settings {
+        scale: quicksilver::graphics::ImageScaleStrategy::Blur,
+        ..Default::default()
+    };
+
+    run::<Game>("Quicksilver Roguelike", Vector::new(800,600), settings);
+}
