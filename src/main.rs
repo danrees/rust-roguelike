e639125453576f849757f174fefa595f7906b70d
Daniel Rees <daniel.rees@autodata.net>
2019-11-13 16:32:18 -0500

    tiles added, next is entities

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,34 @@ use quicksilver::{
     Future, Result,
 };
 
+#[derive(Clone, Debug, PartialEq)]
+struct Tile {
+    pos: Vector,
+    glyph: char,
+    color: Color,
+}
+
+fn generate_map(size: Vector) -> Vec<Tile> {
+    let width = size.x as usize;
+    let height = size.y as usize;
+    let mut map = Vec::with_capacity(width*height);
+
+    for x in 0..width {
+        for y in 0..height {
+            let mut tile = Tile {
+                pos: Vector::new(x as f32, y as f32),
+                glyph: '.',
+                color: Color::BLACK,
+            };
+            if x == 0 || x == width -1 || y == 0 || y == height - 1 {
+                tile.glyph = '#';
+            }
+            map.push(tile);
+        }
+    }
+    map
+}
+
 struct Game {
     title: Asset<Image>,
     mononoki_font_info: Asset<Image>,
