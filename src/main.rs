f204c9d8467351c105cc640c097746586a29a9e1
Daniel Rees <daniel.rees@autodata.net>
2019-11-14 09:33:41 -0500

    up to adding square font

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 //use quicksilver::lifecycle::{Window, Event};
 
 use quicksilver::{
-    geom::{Shape,Vector},
-    graphics::{Background::Img, Color, Font, FontStyle, Image},
+    geom::{Rectangle, Shape,Vector},
+    graphics::{Background::Img, Background::Blended, Color, Font, FontStyle, Image},
     lifecycle::{run, Asset, Settings, State, Window},
     Future, Result,
 };
+use std::collections::HashMap;
+
 
 #[derive(Clone, Debug, PartialEq)]
 struct Tile {
@@ -36,9 +38,57 @@ fn generate_map(size: Vector) -> Vec<Tile> {
     map
 }
 
+#[derive(Clone, Debug, PartialEq)]
+struct Entity {
+    pos: Vector,
+    glyph: char,
+    color: Color,
+    hp: i32,
+    max_hp: i32,
+}
+
+fn generate_entities() -> Vec<Entity> {
+    vec![
+        Entity{
+            pos: Vector::new(9,6),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity{
+            pos: Vector::new(2,4),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity{
+            pos: Vector::new(7,5),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+        Entity{
+            pos: Vector::new(4,8),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        }
+    ]
+}
+
 struct Game {
     title: Asset<Image>,
     mononoki_font_info: Asset<Image>,
+    map_size: Vector,
+    map: Vec<Tile>,
+    entities: Vec<Entity>,
+    player_id: usize,
+    tileset: Asset<HashMap<char,Image>>,
+    tile_size_px: Vector,
 }
 
 impl State for Game {
@@ -55,9 +105,46 @@ impl State for Game {
                 &FontStyle::new(20.0, Color::BLACK),
             )
         }));
+
+        let map_size = Vector::new(20,15);
+        let map = generate_map(map_size);
+        let mut entities = generate_entities();
+
+        let player_id = entities.len();
+        entities.push(
+            Entity {
+                pos: Vector::new(5,3),
+                glyph: '@',
+                color: Color::BLUE,
+                hp: 3,
+                max_hp: 5,
+            }
+        );
+
+        let game_glyphs = "#@g.%";
+        let tile_size_px = Vector::new(12,24);
+
+        let tileset = Asset::new(Font::load(font_mononoki).and_then(move |text| {
+            let tiles = text
+                .render(game_glyphs, &FontStyle::new(tile_size_px.y, Color::WHITE))
+                .expect("Could not render font tileset");
+            let mut tileset = HashMap::new();
+            for (index,glyph) in game_glyphs.chars().enumerate() {
+                let pos = (index as i32 * tile_size_px.x as i32, 0);
+                let tile = tiles.subimage(Rectangle::new(pos,tile_size_px));
+                tileset.insert(glyph,tile);
+            }
+            Ok(tileset)
+        }));
         Ok(Self{
             title,
             mononoki_font_info,
+            map_size,
+            map,
+            entities,
+            player_id,
+            tileset,
+            tile_size_px,
         })
     }
 
@@ -82,6 +169,22 @@ impl State for Game {
                 &image.area().translate((2, window.screen_size().y as i32 - 60)),
                 Img(&image),
             );
+            Ok(())
+        })?;
+
+        let tile_size_px = self.tile_size_px;
+        let offset_px = Vector::new(50, 120);
+        let (tileset,map) = (&mut self.tileset, &self.map);
+        tileset.execute(|tileset| {
+            for tile in map.iter() {
+                if let Some(image) = tileset.get(&tile.glyph) {
+                    let pos_px = tile.pos.times(tile_size_px);
+                    window.draw(
+                        &Rectangle::new(offset_px+pos_px, image.area().size()),
+                        Blended(&image, tile.color),
+                    )
+                }
+            }
             Ok(())
         })?;
 
